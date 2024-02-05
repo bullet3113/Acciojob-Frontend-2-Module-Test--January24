57aaab537bd148aaf2d8ac1655c6921e54af97aa
bullet3113 <0hitesh.a@gmail.com>
2024-02-05 10:33:25 +0530

    priority-progress done

--- script.js
+++ script.js
@@ -17,8 +17,9 @@ let data = [];
 addBtn.addEventListener('click', function () {
     let task = inputTask.value;
     let tId = getTaskId();
-    data.push({"tId": tId, "taskName": task});
+    data.push({"tId": tId, "taskName": task, "priority": "high", "progress" : "todo"});
     renderElements();
+    inputTask.value = "";
 })
 
 function renderElements() {
@@ -26,14 +27,26 @@ function renderElements() {
     data.forEach((e) => {
         let task = e.taskName;
         let tId = e.tId;
+        let priority = e.priority;
+        let progress = e.progress;
+
+        let priorityBtn = priority == 'high' ? `<span id=${tId} onclick="changePriority(${tId})" type="button" class="text-danger fw-semibold me-1">H</span>` : priority == 'medium' ? `<span id=${tId} onclick="changePriority(${tId})" type="button" class="text-warning fw-semibold me-1">M</span>` : `<span id=${tId} onclick="changePriority(${tId})" type="button" class="text-success fw-semibold me-1">L</span>`;
+
+        let progressBtn = progress == "todo" ? `<span id=${tId} onclick="changeProgress(${tId})" type="button" class="text-danger fw-semibold me-1">Todo</span>` : progress == "working" ? `<span id=${tId} onclick="changeProgress(${tId})" type="button" class="text-warning fw-semibold me-1">In-progress</span>` : `<span id=${tId} onclick="changeProgress(${tId})" type="button" class="text-success fw-semibold me-1">Done</span>`;
+
         card += `<div class="card mt-2" id=${"card"+tId}>
     <div class="card-body d-flex">
       <input type="checkbox" class="form-check-input me-2" id="doneCheck" />
       <h5 class="card-title" id=${"taskName"+tId}>${task}</h5>
       <div class="position-absolute end-0 me-3">
+      ${priorityBtn}${progressBtn}
         <i class="bi bi-pencil-square" data-bs-toggle="modal"
         data-bs-target="#exampleModal" id=${tId} onclick="editTask(${tId})"></i>
         <i class="bi bi-trash3" id=${tId} onclick="deleteTask(${tId})"></i>
+      </div>
+      <div class="d-block">
+      
+      
       </div>
     </div>
   </div>`;
@@ -87,4 +100,24 @@ function deleteTask(id) {
     let tempData = data.filter((d) => d.tId != id);
     data = tempData;
     renderElements();
+}
+
+function changePriority(id) {
+    data.forEach((d) => {
+        if(d.tId == id) {
+            d.priority = d.priority == 'low' ? 'medium' : d.priority == 'medium' ? 'high' : 'low';
+        }
+    });
+    console.log(data)
+    renderElements();
+}
+
+function changeProgress(id) {
+    data.forEach((d) => {
+        if(d.tId == id) {
+            d.progress = d.progress == 'todo' ? 'working' : d.progress == 'working' ? 'done' : 'todo';
+        }
+    });
+    renderElements();
+    console.log(data)
 }
